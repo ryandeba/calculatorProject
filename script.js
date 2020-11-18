9a0e5c7f45fd582d5b38d922872f49fec88472db
Zach McNulty <72584137+zachlearnscode@users.noreply.github.com>
2020-11-18 13:15:13 -0600

    Fixed calc and decimal issues
    Fixed an issue, so pressing calc multiple times in a row adds num2 to displayed num.
    Fixed an issue where new number could not begin with a decimal

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ let btnPress = []; //To track recent button presses
 //Set display to show 0 by default
 display.innerText = "0";
 
+if (display.innerText.length > 14) {
+  display.innerText = display.innerText.substr(0, 13);
+}
+
 //Declare variables for Calculate function
 let num1, num2, oper;
 
@@ -32,11 +36,13 @@ function calculate(n1, n2, op) {
 for(let key = 0; key < keys.length; key++) {
   keys[key].addEventListener('click', function() {
 
+    console.log(btnPress);
+
     let num = !this.dataset.action; //Determine if button is number
     let action = this.dataset.action; //Determine if button !number
 
     //Track 2 most recent button presses
-    if (btnPress.length <= 1) {
+    if (btnPress.length < 2) {
       btnPress.push(this);
     } else {
       btnPress.shift();
@@ -52,9 +58,21 @@ for(let key = 0; key < keys.length; key++) {
         display.innerText += this.innerText; //Concat next numbers
       }
     } else if (action === "decimal") {
-        if (!display.innerText.match(/\./)) { //Check for existing dec.
+        if (!display.innerText.match(/\./) && //Check for existing dec.
+            btnPress[0].className != "opKey" && //Check user isn't entering new number
+            btnPress[0].id != "calc") { //Check user didn't last press "calc"
           display.innerText += this.innerText;
-        }
+        } else if (btnPress[0].className === "opKey") { //Allow new number to begin with decimal
+            display.innerText = "";
+            display.innerText += "0" + this.innerText;
+        } else if (btnPress[0].id === "calc") { //Clear all data if last button was "calc"
+          num1 = undefined;
+          num2 = undefined;
+          oper = undefined;
+          btnPress = [];
+          display.innerText = "";
+          display.innerText += "0" + this.innerText;
+        }  
     } else if (action === "add" ||
                action === "subtract" ||
                action === "multiply" ||
@@ -73,10 +91,16 @@ for(let key = 0; key < keys.length; key++) {
           num2 = undefined;
           oper = undefined;
           btnPress = [];
+          store = [];
           display.innerText = "0";
       } else if (action === "calculate") {
-          num2 = display.innerText;
-          display.innerText = calculate(num1, num2, oper);
+          if (btnPress[0].id != "calc") {
+            num2 = display.innerText;
+            display.innerText = calculate(num1, num2, oper);
+          } else { //Handle successive "calc"  presses
+              num1 = display.innerText;
+              display.innerText = calculate(num1, num2, oper);
+          }  
       }
   })
 }
